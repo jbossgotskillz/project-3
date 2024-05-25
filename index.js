709d2d167752b3818cecc817350f418c8cebba61
jbossgotskillz <160427209+jbossgotskillz@users.noreply.github.com>
2024-05-25 01:04:16 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import "core-js";
-import App from './app';
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
